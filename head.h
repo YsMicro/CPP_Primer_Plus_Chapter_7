63fec8de357739a6ecd0ea7d0ea1a59c6d4aac41
Vojago <ywh2742450799@gmail.com>
2024-05-14 00:33:55 +0800

    完成Code_03

--- head.h
+++ head.h
@@ -9,4 +9,6 @@
 
 [[maybe_unused]] int code_02();
 
+[[maybe_unused]] int code_03();
+
 #endif //CPP_PRIMER_PLUS_CHAPTER_7_HEAD_H
--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include "head.h"
+#include <cmath>
 
 int main() {
+    using namespace std;
     std::cout << "Hello, CLion!" << std::endl;
-
 //    code_01();
-    code_02();
-
+//    code_02();
+    code_03();
     return 0;
 }
